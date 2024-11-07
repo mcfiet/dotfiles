3dda832c82acfefb56eff247254193766dc4c255
Fiete Scheel <fietescheel@gmail.com>
2024-11-07 18:16:25 +0100

    Copilot Tab

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local cmp = require("cmp")
+
+cmp.setup({
+  mapping = {
+    ["<Tab>"] = cmp.mapping.confirm({ select = true }), -- Tab für Bestätigung
+    ["<C-e>"] = cmp.mapping.close(), -- Beispiel für Schließen mit Ctrl+e
+    ["<CR>"] = cmp.mapping.abort(), -- Deaktiviert Enter für Copilot-Vorschläge
+  },
+})
