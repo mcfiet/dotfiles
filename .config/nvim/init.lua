606a145e6287b442e22a0efe052b40ae3c09c933
Fiete Scheel <fietescheel@gmail.com>
2025-11-26 10:10:17 +0100

    feat: add dings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,37 @@
 require("config.lazy")
+
+vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_compiler_method = "latexmk"
+vim.g.vimtex_quickfix_mode = 0 -- Optional: Verhindert automatisches Öffnen des Quickfix
+vim.g.vimtex_compiler_latexmk = {
+  options = {
+    "-shell-escape",
+    "-verbose",
+    "-file-line-error",
+    "-synctex=1",
+    "-interaction=nonstopmode",
+  },
+}
+
+local lspconfig = require("lspconfig")
+local util = require("lspconfig.util")
+
+lspconfig.eslint.setup({
+  root_dir = function(fname)
+    local found_config =
+      util.root_pattern(".eslintrc", ".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs", "package.json")(fname)
+
+    -- Verhindere Start, wenn keine Konfigurationsdatei
+    if not found_config then
+      return nil
+    end
+
+    -- Verhindere Start, wenn eslint nicht im Projekt installiert
+    local eslint_in_node_modules = vim.fn.glob(found_config .. "/node_modules/.bin/eslint")
+    if eslint_in_node_modules == "" then
+      return nil
+    end
+
+    return found_config
+  end,
+})
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,21 +7,17 @@ vim.opt.fileencoding = "utf-8"
 vim.opt.number = true
 
 vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+vim.opt.autoindent = true -- Automatische Einrückung
+vim.opt.smartindent = true -- Smarte Einrückung
 vim.opt.hlsearch = true
 vim.opt.backup = false
 vim.opt.showcmd = true
 vim.opt.cmdheight = 0
 vim.opt.laststatus = 0
-vim.opt.expandtab = true
-vim.opt.scrolloff = 10
-vim.opt.inccommand = "split"
-vim.opt.ignorecase = true
-vim.opt.smarttab = true
-vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+vim.opt.expandtab = true -- Tabs durch Leerzeichen ersetzen
+vim.opt.smarttab = true -- Smarte Tab-Einrückung
+vim.opt.shiftwidth = 2 -- Anzahl der Leerzeichen bei Einrückung
+vim.opt.tabstop = 2 -- Wie viele Leerzeichen ein Tabulator entspricht
 vim.opt.wrap = false
 vim.opt.backspace = { "start", "eol", "indent" }
 vim.opt.path:append({ "**" })
@@ -33,3 +29,9 @@ vim.opt.mouse = ""
 
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" })
+
+-- Weitere Optionen für noch besseres Einrückverhalten
+vim.opt.smartcase = true -- Verbessert die Groß-/Kleinschreibungserkennung bei der Suche
+vim.opt.autoindent = true -- Automatische Einrückung beibehalten
+vim.opt.copyindent = true -- Kopiert die Einrückung aus der vorherigen Zeile
+vim.opt.preserveindent = true -- Behalte die Einrückung, wenn du in eine neue Zeile springst
